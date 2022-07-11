43227725e3743175a8608fdab326a99e36623a9d
Onlikerop <zhekamegarep@mail.ru>
2022-07-11 23:10:00 +0300

    Added try-except to main

--- Programs/SCPLib/main.py
+++ Programs/SCPLib/main.py
@@ -1,3 +1,4 @@
+import sys
 import sqlalchemy as db
 from logCollector import logCollector
 from dbController import dbController
@@ -8,17 +9,23 @@ import settings
 SYSTEM_NAME = "SCPLibs Debugger"
 SYSTEM_VERSION = "1.0.0"
 
-engine = db.create_engine(settings.SCPDatabase)
-metadata = db.MetaData(engine)
-session = Session(engine, future=True, autoflush=True)
-collector = logCollector()
-controller = dbController(
-    SYSTEM_NAME,
-    SYSTEM_VERSION,
-    engine.connect(),
-    session if not settings.individualObjects else Session(engine, future=True, autoflush=True),
-    collector if not settings.individualObjects else logCollector()
-)
+try:
+    engine = db.create_engine(settings.SCPDatabase)
+    metadata = db.MetaData(engine)
+    session = Session(engine, future=True, autoflush=True)
+    collector = logCollector()
+    controller = dbController(
+        SYSTEM_NAME,
+        SYSTEM_VERSION,
+        engine.connect(),
+        session if not settings.individualObjects else Session(engine, future=True, autoflush=True),
+        collector if not settings.individualObjects else logCollector()
+    )
+except Exception as exception:
+    print("Произошла ошибка!")
+    if hasattr(exception, 'message'):
+        print(exception.message if settings.debug else settings.veiledError)
+    sys.exit()
 
 
 def main():
